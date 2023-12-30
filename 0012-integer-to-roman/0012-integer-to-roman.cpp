2d4226cae68cb0007df3d8e1c55accd16b0595aa
JackieNeo <111479643+JackieNeoCEG@users.noreply.github.com>
2023-12-31 01:26:26 +0800

    Time: 38 ms (5.24%), Space: 13.2 MB (5.49%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.cpp
+++ 0012-integer-to-roman/0012-integer-to-roman.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     string intToRoman(int num) {
+        //Using ordered map, sorted in descending order using greater<int>
         map<int, string, greater<int>> numMap = {
             {1000, "M"},{900, "CM"},{500, "D"},{400, "CD"},
             {100, "C"},{90, "XC"},{50, "L"},{40, "XL"},
@@ -8,11 +9,12 @@ public:
         };
 
         string result;
-
+        
+        //iterates over the whole map to reduce num to 0
         for (const auto& [value, symbol] : numMap) {
-            while (num >= value) {
-                result += symbol;
-                num -= value;
+            while (num >= value) { //finds largest val to minus
+                result += symbol; //concatenates the string
+                num -= value; 
             }
         }
 
