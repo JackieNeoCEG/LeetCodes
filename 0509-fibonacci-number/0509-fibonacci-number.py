bfcf7000a9f9eb038ce5a71ec99bb8e40220ff9e
JackieNeo <111479643+JackieNeoCEG@users.noreply.github.com>
2024-01-01 03:46:05 +0800

    Time: 10 ms (90.00%), Space: 13.1 MB (85.38%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -1,12 +1,27 @@
 class Solution(object):
+    def __init__(self):
+        # Dictionary to store previously calculated Fibonacci values
+        self.memo = {}
+
     def fib(self, n):
         """
         :type n: int
         :rtype: int
         """
+        if n in self.memo:
+            return self.memo[n]
+
         if n == 0:
-            return 0
-        if n == 1 or n == 2:
-            return 1
-        
-        return self.fib(n - 1) + self.fib(n - 2)
+            result = 0
+        elif n == 1 or n == 2:
+            result = 1
+        else:
+            result = self.fib(n - 1) + self.fib(n - 2)
+
+        # Memoize the result
+        self.memo[n] = result
+        return result
+
+
+
+
