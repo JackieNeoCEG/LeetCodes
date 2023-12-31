b5bb9e4084b0bf336b7f4af4621bb2d5241fb368
JackieNeo <111479643+JackieNeoCEG@users.noreply.github.com>
2024-01-01 03:35:19 +0800

    Time: 107 ms (5.29%), Space: 24 MB (7.77%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,21 @@
+import numpy as np
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        if n == 2:
+            return 2
+        
+        dp = np.zeros(n + 1, dtype=int)
+        dp[1] = 1
+        dp[2] = 2
+
+        for i in range(3, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+
+        return dp[n]
+            
