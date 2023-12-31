d2278d6268e06167660e1a13844a2176976ff9f1
JackieNeo <111479643+JackieNeoCEG@users.noreply.github.com>
2024-01-01 03:31:40 +0800

    Time: 2 ms (45.10%), Space: 6.5 MB (34.05%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        //base cases
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        //vector of size n + 1, with all elements set to 0. Used to store results
+        vector<int> dp(n + 1, 0);
+        dp[1] = 1;
+        dp[2] = 2;
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+
+        return dp[n];
+    }
+};
