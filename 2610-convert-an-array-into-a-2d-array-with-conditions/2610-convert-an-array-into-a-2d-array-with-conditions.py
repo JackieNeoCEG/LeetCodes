229492f334fcfe32e13708082396325e756e60f6
JackieNeo <111479643+JackieNeoCEG@users.noreply.github.com>
2024-01-03 00:28:13 +0800

    Time: 63 ms (12.50%), Space: 13.2 MB (80.56%) - LeetHub

--- 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
+++ 2610-convert-an-array-into-a-2d-array-with-conditions/2610-convert-an-array-into-a-2d-array-with-conditions.py
@@ -4,15 +4,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        #keep track of the count of each unique integer
         count = defaultdict(int)
         res = []
         
         for n in nums:
+            #no. of arrays needed based on the count
             row = count[n]
-            if len(res) == row:
+            if len(res) == row: #start a new row to prevent out-of-bounds error
                 res.append([])
                 
-            res[row].append(n)
+            res[row].append(n) #add current integer to array
             count[n] += 1
         return res
         
