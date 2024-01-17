df4c82298bd8fb018d60176d01727448a310f368
JackieNeo <111479643+JackieNeoCEG@users.noreply.github.com>
2024-01-17 16:41:52 +0800

    Time: 6 ms (17.82%), Space: 8.5 MB (85.59%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        unordered_map<int, int> freqMap;
+
+        // Count the frequency of each element
+        for (int num : arr) {
+            freqMap[num]++;
+        }
+        
+        // Create another unordered_map to store the frequency of frequency counts
+        unordered_map<int, int> freqFreq;
+
+        // Count the frequency of frequency counts
+        for (const auto& pair : freqMap) {
+            freqFreq[pair.second]++;
+        }
+        
+        //
+        for (const auto& pair : freqFreq) {
+            if(pair.second > 1) {
+                return false;
+            }
+        }
+        return true;
+        
+    }
+};
