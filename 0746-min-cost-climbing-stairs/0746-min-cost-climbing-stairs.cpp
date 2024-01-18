9c37c035e5c12ec4a76ff082a39f4c356e16c8b7
JackieNeo <111479643+JackieNeoCEG@users.noreply.github.com>
2024-01-18 13:14:43 +0800

    Time: 3 ms (85.51%), Space: 14.1 MB (43.00%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -3,6 +3,7 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
         
+        //iterative approach to skip the higher cost
         for(int i = 2; i < n; i++) {
             cost[i] += min(cost[i - 1], cost[i - 2]);
         }
