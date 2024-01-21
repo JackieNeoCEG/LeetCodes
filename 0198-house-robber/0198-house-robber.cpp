3b3cfe36fc0790dbc8354c59134d1ce6e89995e0
JackieNeo <111479643+JackieNeoCEG@users.noreply.github.com>
2024-01-21 22:36:56 +0800

    Time: 4 ms (25.45%), Space: 9.1 MB (9.67%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         //Keep track of max amt when robbing/not robbing 
-        int rob = 0; int notRob = 0;
+        int robHouse = 0; int notRob = 0;
         for (int i = 0; i < nums.size(); i++) {
             //update new maxes 
             int newRob = notRob + nums[i];
-            int newNoRob = max(notRob, rob);
+            int newNoRob = max(notRob, robHouse);
             
             //update the variables
-            rob = newRob;
+            robHouse = newRob;
             notRob = newNoRob;
         }
         //return max of scenarios
-        return max(rob, notRob);
+        return max(robHouse, notRob);
     }
 };
