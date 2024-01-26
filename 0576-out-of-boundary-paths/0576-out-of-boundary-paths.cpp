5a8f7780feb087783eea567a76d6b561438b6702
JackieNeo <111479643+JackieNeoCEG@users.noreply.github.com>
2024-01-27 00:14:28 +0800

    Time: 56 ms (11.85%), Space: 99.3 MB (5.02%) - LeetHub

---
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    const int MOD = 1000000007;
+    //3D dp vector to store startRow startColumn maxMove
+    vector<vector<vector<int>>> memo;
+    
+    int solve(int n, int m, int maxMove, int startRow, int startColumn) {
+                
+        // Out of bounds condition to return 1 path
+        if (startColumn < 0 || startColumn >= n || startRow < 0 || startRow >= m) {
+            return 1;
+        }
+        
+        // Base case: maxMove reached
+        if (maxMove == 0) {
+            return 0;
+        }
+        // Initialize memoi vector to have dimensions n x m x (maxMove + 1) with all values of -1
+        if (memo.empty()) {
+            memo.resize(50, vector<vector<int>>(50, vector<int>(51, -1)));
+        }
+
+
+        // Check if result is memoized
+        if (memo[startRow][startColumn][maxMove] != -1) {
+            return memo[startRow][startColumn][maxMove];
+        }
+
+        // Recursion step to exhaust maxMove
+        long long up = solve(n, m, maxMove - 1, startRow - 1, startColumn);
+        long long down = solve(n, m, maxMove - 1, startRow + 1, startColumn);
+        long long left = solve(n, m, maxMove - 1, startRow, startColumn - 1);
+        long long right = solve(n, m, maxMove - 1, startRow, startColumn + 1);
+
+        // Calculate the total paths and store in memoization table
+        long long totalPaths = (up + down + left + right) % MOD;
+        memo[startRow][startColumn][maxMove] = totalPaths;
+
+        return totalPaths;
+    }
+    
+    int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        return solve(n, m, maxMove, startRow, startColumn);
+    }
+};
