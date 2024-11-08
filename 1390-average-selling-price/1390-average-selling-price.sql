f778bd9a002570095b2e7691bf9fa0992e30e1ea
Jackie Neo Seh Jie <e0959140@u.nus.edu>
2024-11-08 17:59:03 +0800

    Time: 691 ms (95.27%), Space: 0B (100%) - LeetHub

--- 1390-average-selling-price/1390-average-selling-price.sql
+++ 1390-average-selling-price/1390-average-selling-price.sql
@@ -1,6 +1,8 @@
 # Write your MySQL query statement below
 SELECT 
     p.product_id
+    
+    -- replace null with 0, round to 2dp, averaging formula
     ,IFNULL(ROUND(SUM(u.units * p.price) / SUM(u.units), 2), 0) average_price
 FROM Prices p LEFT JOIN UnitsSold u 
 ON u.product_id = p.product_id AND
