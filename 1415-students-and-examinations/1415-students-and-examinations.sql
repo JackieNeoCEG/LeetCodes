0c4ff443f3355138c9a0ee00990bfa01ccf888ef
Jackie Neo Seh Jie <e0959140@u.nus.edu>
2024-11-08 17:03:58 +0800

    Time: 934 ms (79.69%), Space: 0B (100%) - LeetHub

--- 1415-students-and-examinations/1415-students-and-examinations.sql
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
-SELECT st.student_id, st.student_name, sb.subject_name, COUNT(e.student_id) attended_exams
+SELECT 
+    st.student_id
+    ,st.student_name
+    ,sb.subject_name
+    ,COUNT(e.student_id) attended_exams
 FROM Students st
+
+-- To list all subjects for each student, use cartesian product 
+-- to generate every possible pair of rows from two tables
 CROSS JOIN Subjects sb
 LEFT JOIN Examinations e ON e.student_id = st.student_id
                          AND e.subject_name = sb.subject_name
