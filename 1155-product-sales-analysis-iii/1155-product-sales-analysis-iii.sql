a65e05d5c68d7d90d25f10f0a96c458c30d190bf
Jackie Neo Seh Jie <e0959140@u.nus.edu>
2024-11-12 16:47:46 +0800

    Time: 1023 ms (79.6%), Space: 0B (100%) - LeetHub

--- 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
+++ 1155-product-sales-analysis-iii/1155-product-sales-analysis-iii.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
+-- filters Sales to include only rows where (product_id, year)
+-- match each combination returned by the subquery
 SELECT
     product_id
     ,year AS first_year
@@ -6,6 +8,7 @@ SELECT
     ,price
 FROM Sales
 WHERE (product_id, year) IN (
+        -- Subquery returns minimum year for each product_id
         SELECT 
             product_id
             ,MIN(year)
