9257a4a18595d542c1fd55502e2a6690677b39ab
Jackie Neo Seh Jie <e0959140@u.nus.edu>
2024-11-13 00:22:36 +0800

    Time: 393 ms (88.02%), Space: 0B (100%) - LeetHub

---
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+-- To remove duplicate numbers, we use a subquery to 
+-- generate a new table with single numbers only
+
+SELECT MAX(num) AS num
+FROM (
+    SELECT num
+    FROM MyNumbers
+    GROUP BY num
+    HAVING COUNT(*) = 1
+) AS SingleNumTablw
