28dc8223a263052add629b087ac6ddefe4a43f5e
Jackie Neo Seh Jie <e0959140@u.nus.edu>
2025-09-30 16:37:39 +0800

    Time: 185 ms (27.36%), Space: 144.2 MB (33.64%) - LeetHub

---
+++ 2324-find-triangular-sum-of-an-array/2324-find-triangular-sum-of-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int triangularSum(vector<int>& nums) {
+        int n = nums.size();
+        if (n == 1) return nums[0];
+
+        vector<int> next;
+        next.reserve(n - 1);
+        for (int i = 0; i < n - 1; i++) {
+            next.push_back((nums[i] + nums[i+1]) % 10);
+        }
+
+        return triangularSum(next);
+
+        
+
+    }
+};
